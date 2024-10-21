cbe888fe923c0b09fd5e0bca94173e3ea5b2eb3b
Axel Pestoni <axelkpesto@gmail.com>
2024-10-21 12:55:31 -0700

    Example Requests

--- VECTOR_DB_SERVER.py
+++ VECTOR_DB_SERVER.py
@@ -8,32 +8,41 @@ app = Flask(__name__)
 vd = VectorDatabase()
 vd.parse_json("AllPrintings.json",runtime=True,max_lines=2500)
 
-@app.route('/add_card', methods=['POST'])
-def add_card():
-    card_data = request.json # Assume card data comes in JSON format
-    card = Card(card_data)
-    vd.add_card(card)
-    return jsonify({"message": "Card added"}), 201
-
+#Example Request:
+#(Invoke-RestMethod -Uri "http://127.0.0.1:5000/get_vector/Shunt")
 @app.route('/get_vector/<string:v_id>', methods=['GET'])
 def get_vector(v_id):
     vector = vd.get_vector(v_id)
     return jsonify({"vector": vector.tolist() if vector is not None else None})
 
+#Example Request:
+#curl http://127.0.0.1:5000/get_random_vector
 @app.route('/get_random_vector', methods=['GET'])
 def get_random_vector():
     random_vector = vd.get_random_vector()
     return jsonify({"id":random_vector[0], "vector": random_vector[1]})
 
+#Example Request:
+#curl http://127.0.0.1:5000/get_random_vector_description
 @app.route('/get_random_vector_description', methods=['GET'])
 def get_random_vector_description():
     return jsonify(vd.get_vector_description_dict(vd.get_random_vector()[0]))
 
+#Example Request:
+#(Invoke-RestMethod -Uri "http://127.0.0.1:5000/get_similar_vectors/Shunt?num_vectors=10")
 @app.route('/get_similar_vectors/<string:v_id>', methods=['GET'])
-def get_similar_vectors():
-    q_vector = request.json.get('vector')
-    n_results = request.json.get('n_results', 5)
-    results = vd.get_similar_vectors(np.array(q_vector), n_results)
+def get_similar_vectors(v_id):
+    try:
+        num_vectors = request.args.get('num_vectors', default=5, type=int)
+    except ValueError:
+        return jsonify({"error": "num_vectors must be an integer"}), 400
+    
+    results_list = vd.get_similar_vectors(vd.get_vector(v_id=v_id), num_vectors)
+    
+    results = {}
+    for i in range(len(results_list)):
+        results[str(i)] = vd.get_vector_description_dict(results_list[i][0])
+
     return jsonify(results)
 
 if __name__ == '__main__':
